524a8d1381bf9b94a0367c9a10108c5d00452f37
NamesAlice <yuan.alice93@gmail.com>
2015-09-20 02:41:41 -0400

    pay url

--- icontrib/urls.py
+++ icontrib/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^logout/?$', 'icontrib.views.logout', name='logout'),
 
-    url(r'^setup_payment/?$', 'icontrib.views.setup_payments', name='setup_payment'),
+    url(r'^pay/?$', 'icontrib.views.setup_payments', name='setup_payment'),
 
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^payments/', include('payments.urls', namespace='payments'))
